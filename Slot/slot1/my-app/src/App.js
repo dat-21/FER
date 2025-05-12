a579c452f5fc2f83db1c3b738cb36e6a9bb57955
datdeptrainhatthegioi <tiendatyyy2005@gmail.com>
2025-05-12 17:28:03 +0700

    first commit

---
+++ Slot/slot1/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <p>
+        This is a simple React application that demonstrates the use of components and state management.
+      </p>
+    </div>
+  );
+}
+
+export default App;
